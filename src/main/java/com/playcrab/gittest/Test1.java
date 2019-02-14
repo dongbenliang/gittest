19e594d4cd3bbc9fb34f9cb72bf619ef8df1fb8b
dongbenliang <dongbenliang@playcrab.com>
2019-02-14 16:57:46 +0800

    branch commit 9004

--- src/main/java/com/playcrab/gittest/Test1.java
+++ src/main/java/com/playcrab/gittest/Test1.java
@@ -5,6 +5,6 @@ public class Test1 {
 		System.out.println("Test3");
 		System.out.println("b1修改");
 		System.out.println("b4修改");
-		System.out.println("b21修改");
+		System.out.println("b22修改");
 	}
 }
