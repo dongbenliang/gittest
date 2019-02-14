ba0348d053db219105bf2bc4d5ca54a062802070
dongbenliang <dongbenliang@playcrab.com>
2019-02-14 11:17:30 +0800

    kkkkkkkkkkkk

--- src/main/java/com/playcrab/gittest/Test1.java
+++ src/main/java/com/playcrab/gittest/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test3");
 		System.out.println("b1修改");
-		System.out.println("b2修改");
+		System.out.println("b3修改");
 	}
 }
