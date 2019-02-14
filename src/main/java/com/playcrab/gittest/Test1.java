ef357c0640c7ce794ffa965e12db573828d0c266
dongbenliang <42536715+dongbenliang@users.noreply.github.com>
2019-02-14 19:46:21 +0800

    Update Test1.java

--- src/main/java/com/playcrab/gittest/Test1.java
+++ src/main/java/com/playcrab/gittest/Test1.java
@@ -5,6 +5,6 @@ public class Test1 {
 		System.out.println("Test3");
 		System.out.println("b1修改");
 		System.out.println("b4修改");
-		System.out.println("b26修改");
+		System.out.println("b27修改");
 	}
 }
