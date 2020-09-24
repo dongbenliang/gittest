d896d219aa86795bd5d23f55058859217e42fa15
dongbenliang <dongbenliang@playcrab.com>
2020-09-24 10:16:49 +0800

    ddd

--- src/main/java/com/playcrab/gittest/App.java
+++ src/main/java/com/playcrab/gittest/App.java
@@ -4,10 +4,8 @@ package com.playcrab.gittest;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World24!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World25!");
     }
 }
