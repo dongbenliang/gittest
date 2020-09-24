0c47097f384624ef6ab7e84157fd5ba8d9ca3281
dongbenliang <dongbenliang@playcrab.com>
2020-09-24 10:09:54 +0800

    ssss

--- src/main/java/com/playcrab/gittest/Test1.java
+++ src/main/java/com/playcrab/gittest/Test1.java
@@ -1,30 +1,30 @@
 package com.playcrab.gittest;
 
 public class Test1 {
-	public static void main(String[] args) {
-		System.out.println("Test3");
-		System.out.println("b1修改");
-		System.out.println("b4修改");
-		System.out.println("b31===================================修改");
-		System.out.println("ccc===================================修改");
-		System.out.println("ddd===================================修改");
-		System.out.println("eee===================================修改");
-		System.out.println("fff===================================修改");
-		System.out.println("ggg===================================修改");
-		System.out.println("kkk===================================修改");
-		System.out.println("ooo===================================修改");
-		System.out.println("jjj===================================修改");
-		System.out.println("mmm===================================修改");
-		System.out.println("nnn===================================修改");
-		System.out.println("zzz===================================修改");
-		System.out.println("ppp===================================修改");
-		System.out.println("ttt===================================修改");
-		System.out.println("ttt3===================================修改");
-		System.out.println("ttt4===================================修改");
-		System.out.println("ttt5===================================修改");
-		System.out.println("ttt16===================================修改");
-		System.out.println("ttt20===================================修改");
-		System.out.println("ttt21===================================修改");
-		System.out.println("ttt26===================================修改");
-	}
+    public static void main(String[] args) {
+        System.out.println("Test4");
+        System.out.println("b1修改");
+        System.out.println("b4修改");
+        System.out.println("b31===================================修改");
+        System.out.println("ccc===================================修改");
+        System.out.println("ddd===================================修改");
+        System.out.println("eee===================================修改");
+        System.out.println("fff===================================修改");
+        System.out.println("ggg===================================修改");
+        System.out.println("kkk===================================修改");
+        System.out.println("ooo===================================修改");
+        System.out.println("jjj===================================修改");
+        System.out.println("mmm===================================修改");
+        System.out.println("nnn===================================修改");
+        System.out.println("zzz===================================修改");
+        System.out.println("ppp===================================修改");
+        System.out.println("ttt===================================修改");
+        System.out.println("ttt3===================================修改");
+        System.out.println("ttt4===================================修改");
+        System.out.println("ttt5===================================修改");
+        System.out.println("ttt16===================================修改");
+        System.out.println("ttt20===================================修改");
+        System.out.println("ttt21===================================修改");
+        System.out.println("ttt26===================================修改");
+    }
 }
