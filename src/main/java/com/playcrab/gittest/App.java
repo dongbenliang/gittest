ca2f567654cc8d79e542de591f95aea40422b8bd
dongbenliang <dongbenliang@playcrab.com>
2021-08-20 12:27:17 +0800

    ===============

--- src/main/java/com/playcrab/gittest/App.java
+++ src/main/java/com/playcrab/gittest/App.java
@@ -7,5 +7,6 @@ package com.playcrab.gittest;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World25!");
+        System.out.println("commit1");
     }
 }
