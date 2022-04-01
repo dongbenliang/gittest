46c70eb5b4bd3b1a42ff0af60d516fb1e8d36eac
dongbenliang <42536715+dongbenliang@users.noreply.github.com>
2022-04-01 18:55:45 +0800

    Update App.java

--- src/main/java/com/playcrab/gittest/App.java
+++ src/main/java/com/playcrab/gittest/App.java
@@ -7,7 +7,7 @@ package com.playcrab.gittest;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World25!");
-        System.out.println("commit2");
+        System.out.println("commit4");
         System.out.println("commit3");
     }
 }
